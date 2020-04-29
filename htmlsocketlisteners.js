005d50c3faefbb59df390eb44e8680409617d2a7
Devin Warrick <dev.warr@gmail.com>
2020-04-29 16:41:17 -0500

    Adds Chatroom baic styling

--- htmlsocketlisteners.js
+++ htmlsocketlisteners.js
@@ -1,17 +1,38 @@
+scoreboard = {
+    highscores: [{},{},{}],
+    player: {}
+}
+
+socket.on("highscoreupdate", data => {
+    scoreboard.highscores[0].textContent = data[0]
+    scoreboard.highscores[1].textContent = data[1]
+    scoreboard.highscores[2].textContent = data[2]
+})
+
+sokcet.on("playerupdate", player => {
+    scoreboard.player.textContent = player.score
+})
+
 /**
  * finds the three span elements that hold the top three scores
  * and adds a socket listener to set the usernames and scores
  */
 function listenForInfo() {
-    highscores = Array.from(document.getElementsByClassName("highscore"))
-    socket.on("highscoreupdate", data => {
-        highscores[0].textContent = data[0]
-        highscores[1].textContent = data[1]
-        highscores[2].textContent = data[2]
-    })
-    // Sanity check to make sure text content changes
-    // highscores[0].textContent = "data[0]"
-    // highscores[1].textContent = "data[1]"
-    // highscores[2].textContent = "data[2]"
+    scoreboard.highscores = Array.from(document.getElementsByClassName("highscore"))
+    scoreboard.player = document.getElementById("player-score")
+    scoreboard.highscores[0].textContent = "Melanoma: 99800"
+    scoreboard.highscores[1].textContent = "6k6: 12500"
+    scoreboard.highscores[2].textContent = "Dank: 8400"
+    scoreboard.player.textContent = "0"
 
 }
+
+
+chatContainer = {
+    div: {},
+    children: [{}]
+}
+
+socket.on("roomupdate", ({chat}) => {
+    
+})
