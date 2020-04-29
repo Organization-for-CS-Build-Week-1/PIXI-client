5a8f5f585451d4fd07b3f09f145934347d2023a5
Devin Warrick <dev.warr@gmail.com>
2020-04-29 18:55:18 -0500

    Adds inventory socket listeners and function

--- htmlsocketlisteners.js
+++ htmlsocketlisteners.js
@@ -1,5 +1,10 @@
 // Create an empty socket so the code doesn't break before io connects
-var socket = { on: () => {}, emit: () => {} }
+/** Empty Function as a placeholder */
+const eF = () => {}
+
+var socket = { on: eF, emit: eF }
+
+// ==================== HIGHSCORE SETUP ==================== //
 
 const scoreboardInitState = {
   highscores: [{}, {}, {}],
@@ -8,75 +13,171 @@ const scoreboardInitState = {
 
 scoreboard = scoreboardInitState
 
-socket.on('highscoreupdate', (data) => {
-  scoreboard.highscores[0].textContent = data[0]
-  scoreboard.highscores[1].textContent = data[1]
-  scoreboard.highscores[2].textContent = data[2]
-})
+const mockScores = ['Melanoma: 99800', '6k6: 12500', 'Dank: 8400']
 
-socket.on('playerupdate', (player) => {
-  scoreboard.player.textContent = player.score
-})
+function updateHighscores(scores) {
+  scoreboard.highscores[0].textContent = scores[0]
+  scoreboard.highscores[1].textContent = scores[1]
+  scoreboard.highscores[2].textContent = scores[2]
+}
 
-/**
- * finds the three span elements that hold the top three scores
- * and adds a socket listener to set the usernames and scores
- */
-function highscoreSetup() {
+function scoreboardSetup() {
   const highscores = Array.from(document.getElementsByClassName('highscore'))
   if (highscores) scoreboard.highscores = highscores
   scoreboard.player = document.getElementById('player-score')
-  scoreboard.highscores[0].textContent = 'Melanoma: 99800'
-  scoreboard.highscores[1].textContent = '6k6: 12500'
-  scoreboard.highscores[2].textContent = 'Dank: 8400'
+  // Testing
+  updateHighscores(mockScores)
   scoreboard.player.textContent = '0'
 }
 
+// ==================== CHATROOM SETUP ==================== //
+
 const chatContainerInitState = {
-  div: { children: {} },
+  children: [{ remove: eF }],
+  append: eF,
+  remove: eF,
 }
 chatContainer = chatContainerInitState
 
-socket.on('roomupdate', ({ chat }) => {
-  const newMessage = document.createElement('p')
+const mockChatMessages = [
+  'Dank entered the room',
+  'Dank: Hi!',
+  'Dank took some trash',
+  'Dank took a hammer',
+  'Dank dropped a stick',
+  'Dank dropped a stick',
+  'Dank took a gem',
+  'Dank dropped a stick',
+  'Dank dropped a stick',
+  'Dank dropped a stick',
+  '6k6: ...Dude.',
+  'Dank dropped a stick',
+  'Dank dropped a stick',
+  'Dank took a gem',
+  '6k6: GG',
+]
 
-  if (/: .*(...Dude.|Hi!|GG)/.test(chat)) {
+function updateChat(chatMessage) {
+  const pTag = document.createElement('p')
+
+  if (/: .*(...Dude.|Hi!|GG)/.test(chatMessage)) {
     // If the incoming message has one of the template chats,
     // put the chat message in a span
-    chat = chat.split(':')
-    newMessage.textContent = chat[0]
-    const chatMessage = document.createElement('span')
-    chatMessage.textContent = chat[1]
+    chatMessage = chatMessage.split(':')
+    pTag.textContent = chatMessage[0]
+    const spanTag = document.createElement('span')
+    spanTag.textContent = chatMessage[1]
+    pTag.append(spanTag)
   }
   // Otherwise, just stick it all together
-  else newMessage.textContent = chat
+  else pTag.textContent = chatMessage
 
-  if (chatContainer.div.append) {
-    chatContainer.div.append(newMessage)
+  if (chatContainer.append) {
+    chatContainer.append(pTag)
   }
-  if (chatContainer.div.children.length > 15) {
-    chatContainer.div.children[0].remove()
+  if (chatContainer.children.length > 15) {
+    chatContainer.children[0].remove()
   }
-})
+}
 
 function chatSend(e) {
   e.preventDefault()
   console.log(e.target.textContent)
-  //   socket.emit('chat', e.target.textContent)
+  socket.emit('chat', e.target.textContent)
 }
 
 function chatSetup() {
-    
   const chatDiv = document.getElementById('chat-container')
-  if (chatDiv) chatContainer.div = chatDiv
+  if (chatDiv) chatContainer = chatDiv
 
   const chatOptionButtons = document.querySelectorAll('.chat-options button')
   chatOptionButtons.forEach((button) => {
     button.addEventListener('click', chatSend)
   })
+
+  // Testing
+
+
+mockChatMessages.map(updateChat)
 }
 
+// ==================== INVENTORY SETUP ==================== //
+
+playerInventory = {}
+
+class ItemContainer {
+  constructor({ id, name, weight, score }) {
+    this.id = id
+    this.name = name
+    this.weight = weight
+    this.score = score
+    this.div = this.createDiv()
+    this.div.addEventListener('click', this.dropItem)
+  }
+
+  createDiv = () => {
+    const item = document.createElement('div')
+    item.className = 'item'
+
+    const imgDiv = document.createElement('div')
+    const weightDiv = document.createElement('div')
+    const scoreDiv = document.createElement('div')
+    imgDiv.className = weightDiv.className = scoreDiv.className = 'third'
+
+    const img = document.createElement('img')
+    img.src = `assets/PNG/${this.name}.png`
+    img.alt = this.name
+    const weight = document.createElement('h4')
+    weight.textContent = this.weight
+    const score = document.createElement('h4')
+    score.textContent = this.score
+
+    imgDiv.append(img)
+    weightDiv.append(weight)
+    scoreDiv.append(score)
+    item.append(imgDiv, weightDiv, scoreDiv)
+    return item
+  }
+
+  dropItem = () => {
+    console.log(this.id)
+    socket.emit('drop', this.id)
+  }
+}
+
+const mockItems = [
+  { id: 0, name: 'Gem', weight: 25, score: 4500 },
+  { id: 0, name: 'Gem', weight: 25, score: 4500 },
+  { id: 0, name: 'Gem', weight: 25, score: 4500 },
+  { id: 0, name: 'Gem', weight: 25, score: 4500 },
+]
+
+function updateInventory(items) {
+  const currentItems = items.map((item) => new ItemContainer(item))
+  playerInventory.innerHTML = ''
+  currentItems.forEach((item) => playerInventory.append(item.div))
+}
+
+function inventorySetup() {
+  playerInventory = document.getElementById('inventory-container')
+  console.log(playerInventory.children)
+  // Testing
+  updateInventory(mockItems)
+}
+
+// ==================== SOCKET.ON ==================== //
+
+socket.on('roomupdate', updateChat)
+
+socket.on('highscoreupdate', updateHighscores)
+
+socket.on('playerupdate', (player) => {
+  scoreboard.player.textContent = player.score
+  updateInventory(player.items)
+})
+
 function listenForInfo() {
-  highscoreSetup()
+  scoreboardSetup()
   chatSetup()
+  inventorySetup()
 }
